973457f6750e42264e08c3c2cac32f0a1f90c091
Ramon Prata <c0009886@vli-logistica.com.br>
2021-02-10 20:58:44 -0300

    Aula 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './DemoRenderizacao';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Hello />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/DemoRenderizacao.jsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <div>Hello world!</div>;
+};
+
+export default Hello;
