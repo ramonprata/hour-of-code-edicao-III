af047c4f7c9db2f33e19a4b6315c60030110ec6e
Ramon Prata <c0009886@vli-logistica.com.br>
2021-03-09 18:03:40 -0300

    Aula VII

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { getMaterialUItheme } from './shared/theme';
 
 function App() {
-  const themeType = 'light';
+  const themeType = 'dark';
 
   const materialUItheme = getMaterialUItheme(themeType);
 
