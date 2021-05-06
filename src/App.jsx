c86b1e358d3015a15f776b0bb04e754e56f893ae
Ramon Prata <c0009886@vli-logistica.com.br>
2021-05-06 14:55:47 -0300

    Componentização

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,18 @@ import {
   MaterialUIStyleComponent,
   CssModuleComponent,
 } from './aulaIII';
-import { WelcomeDialog, SplitPane, Chat, Contacts, Feed } from './aulaIV';
+import { WelcomeDialog, SplitPane, Chat, Contacts, Feed, FancyBorder } from './aulaIV';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="content">
-          <SplitPane rightContent={<Chat />} leftContent={<Contacts />}>
-            <Feed />
-          </SplitPane>
+          <FancyBorder>
+            <SplitPane leftContent={<Chat />} rightContent={<Contacts />}>
+              <Feed />
+            </SplitPane>
+          </FancyBorder>
         </div>
       </header>
     </div>
